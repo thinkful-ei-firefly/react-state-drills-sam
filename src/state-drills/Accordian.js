acb743d8fd1a09999541c09ef9e917700b199e94
Sam Walker <jack.nasal@gmail.com>
2019-07-31 15:20:18 -0400

    added git add .

--- src/state-drills/Accordian.js
+++ src/state-drills/Accordian.js
@@ -19,7 +19,7 @@ class Accordian extends React.Component {
     return (
       <div className='accordian'>
         <ul>
-          {this.props.sections && this.props.sections.map((item, index) => { return (
+          {!!this.props.sections && this.props.sections.map((item, index) => { return (
             <li key={index}>
               <button onClick={()=>this.handleClick(index)}>{item.title}</button>
               <p>{this.state.openItem===index && item.content}</p>
